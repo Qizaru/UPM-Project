6d3539af950f3254cb860faa20c494bde36026a4
Qizaru <148689017+Qizaru@users.noreply.github.com>
2023-11-02 16:12:24 +0800

    newer one Kah Meng check please

--- Game/src/com/zetcode/LeaderboardManager.java
+++ Game/src/com/zetcode/LeaderboardManager.java
@@ -2,69 +2,76 @@ package com.zetcode;
 
 import javax.swing.*;
 import java.awt.Font;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.util.List;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class LeaderboardManager {
-    public static boolean displayLeaderboard(int highScore) {
+    public static boolean saver = false;
+
+    public static boolean displayLeaderboard(int highScore, String playerName) {
         JFrame frame = new JFrame("Leaderboard");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
         List<String> leaderboardData = loadLeaderboardData("leaderboard.txt");
 
-        String playerName = JOptionPane.showInputDialog("Enter your name: ");
-        int playerScore = highScore;
+        String newEntry = playerName + ": " + highScore;
 
-        String newEntry = playerName + ": " + playerScore;
-        leaderboardData.add(newEntry);
+        boolean scoreAdded = false;
 
-        Collections.sort(leaderboardData, (s1, s2) -> {
-            int score1 = Integer.parseInt(s1.split(": ")[1]);
-            int score2 = Integer.parseInt(s2.split(": ")[1]);
-            return Integer.compare(score2, score1); // Descending order
-        });
+        if (leaderboardData.isEmpty() || leaderboardData.size() < 5) {
+            leaderboardData.add(newEntry);
+            scoreAdded = true;
+        } else {
+            int lowestScore = Integer.MAX_VALUE;
+            int lowestScoreIndex = -1;
 
-        int maxEntries = 5;
-        if (leaderboardData.size() > maxEntries) {
-            leaderboardData = leaderboardData.subList(0, maxEntries);
-        }
-
-        DefaultListModel<String> leaderboardModel = new DefaultListModel<>();
-        for (String entry : leaderboardData) {
-            leaderboardModel.addElement(entry);
-        }
-
-        JList<String> leaderboardList = new JList<>(leaderboardModel);
-
-        // Use a smaller font size
-        leaderboardList.setFont(new Font("Arial", Font.PLAIN, 14));
-
-        frame.add(new JScrollPane(leaderboardList));
-        
-        frame.setLocation(650,320 );
-        ImageIcon licon = new ImageIcon("src/resources/images/licon.png");
-        frame.setIconImage(licon.getImage());
-        
-        frame.setSize(200, 200);
-        frame.setVisible(true);
-
-        final boolean[] leaderboardClosed = {false}; // Create an array to hold the flag
-
-        frame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosed(WindowEvent e) {
-                frame.dispose(); // Close the leaderboard window
-                leaderboardClosed[0] = true; // Set the flag to indicate the leaderboard was closed
+            for (int i = 0; i < leaderboardData.size(); i++) {
+                int score = Integer.parseInt(leaderboardData.get(i).split(": ")[1]);
+                if (score < lowestScore) {
+                    lowestScore = score;
+                    lowestScoreIndex = i;
+                }
             }
-        });
 
-        saveLeaderboardData(leaderboardData, "leaderboard.txt");
+            if (highScore > lowestScore) {
+                leaderboardData.remove(lowestScoreIndex);
+                leaderboardData.add(newEntry);
+                scoreAdded = true;
+            }
+        }
 
-        return leaderboardClosed[0];
+        if (scoreAdded) {
+            Collections.sort(leaderboardData, (s1, s2) -> {
+                int score1 = Integer.parseInt(s1.split(": ")[1]);
+                int score2 = Integer.parseInt(s2.split(": ")[1]);
+                return Integer.compare(score2, score1); // Descending order
+            });
+
+            DefaultListModel<String> leaderboardModel = new DefaultListModel<>();
+            for (String entry : leaderboardData) {
+                leaderboardModel.addElement(entry);
+            }
+
+            JList<String> leaderboardList = new JList<>(leaderboardModel);
+
+            // Use a smaller font size
+            leaderboardList.setFont(new Font("Arial", Font.PLAIN, 14));
+
+            frame.add(new JScrollPane(leaderboardList));
+            frame.setLocation(650, 320);
+            ImageIcon licon = new ImageIcon("src/resources/images/licon.png");
+            frame.setIconImage(licon.getImage());
+            frame.setSize(200, 200);
+            frame.setVisible(true);
+
+            saver = saveLeaderboardData(leaderboardData, "leaderboard.txt");
+        } else {
+            JOptionPane.showMessageDialog(null, "Sorry, your score is too low!!");
+        }
+
+        return true;
     }
 
     private static List<String> loadLeaderboardData(String fileName) {
@@ -72,12 +79,12 @@ public class LeaderboardManager {
         try {
             File file = new File(fileName);
             if (file.exists()) {
-                BufferedReader reader = new BufferedReader(new FileReader(file));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    leaderboardData.add(line);
+                try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        leaderboardData.add(line);
+                    }
                 }
-                reader.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -85,17 +92,19 @@ public class LeaderboardManager {
         return leaderboardData;
     }
 
-    private static void saveLeaderboardData(List<String> leaderboardData, String fileName) {
+    private static boolean saveLeaderboardData(List<String> leaderboardData, String fileName) {
         try {
             File file = new File(fileName);
-            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-            for (String entry : leaderboardData) {
-                writer.write(entry);
-                writer.newLine();
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+                for (String entry : leaderboardData) {
+                    writer.write(entry);
+                    writer.newLine();
+                }
             }
-            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
+            return false;
         }
+        return true;
     }
 }
